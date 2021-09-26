c126abc309a3a4329690b7e8066e3eb20010e38e
Bogachev <bogachok-m@mail.ru>
2021-09-26 19:43:10 +0300

    Initial commit

---
+++ NotesCFTApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesCFTApp.swift
+//  NotesCFT
+//
+//  Created by mac on 26.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct NotesCFTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
