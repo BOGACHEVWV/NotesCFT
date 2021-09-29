8d022b6a475c12fa022b807137b923020bb840b4
Bogachev <bogachok-m@mail.ru>
2021-09-29 19:10:26 +0300

    delete chevron

--- Views/MainView.swift
+++ Views/MainView.swift
@@ -11,10 +11,19 @@ struct MainView: View {
         
     var body: some View {
         NavigationView  {
-            List(0..<20, id: \.self) { item in
-                NavigationLink(destination: NoteView()) {
-                    noteCell(item: item)
+            List {
+                ForEach(0..<20) { item in
+                    ZStack {
+                        noteCell(item: item)
+                        NavigationLink(destination: NoteView()) {
+                            EmptyView()
+                        }
+                        .hidden()
+                        
+                    }
+                    
                 }
+                
                 
             }
             
@@ -24,6 +33,7 @@ struct MainView: View {
                     .font(.title)
             })
         }
+//        .buttonStyle(PlainButtonStyle())
         .navigationAppearance(backgroundColor: .systemRed, foregroundColor: .systemBackground, tintColor: .systemBackground, hideSeparator: true)
         .navigationViewStyle(StackNavigationViewStyle())
         
@@ -39,29 +49,32 @@ struct MainView_Previews: PreviewProvider {
 struct noteCell: View {
     var item: Int
     var body: some View {
-        VStack {
-            HStack {
-                Text("Text  \(item)")
-                    .lineLimit(1)
-                    .minimumScaleFactor(0.9)
-                    .font(.headline)
+     
+            
+            VStack {
+                HStack {
+                    Text("Text  \(item)")
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.9)
+                        .font(.headline)
+                    Spacer()
+                    
+                }
                 Spacer()
-                
+                HStack {
+                    Text("28.09.2021")
+                    Text("20:20")
+                    Spacer()
+                    // если массив с карниками не пуст
+                    Image(systemName: "photo.on.rectangle")
+                    //                            .resizable()
+                    //                            .scaledToFit()
+                }
+                .font(.footnote)
+                .foregroundColor(.secondary)
             }
-            Spacer()
-            HStack {
-                Text("28.09.2021")
-                Text("20:20")
-                Spacer()
-                // если массив с карниками не пуст
-                Image(systemName: "photo.on.rectangle")
-                //                            .resizable()
-                //                            .scaledToFit()
-            }
-            .font(.footnote)
-            .foregroundColor(.secondary)
-        }
-        .padding(.vertical, 5)
+            .padding(.vertical, 5)
+        
     }
 }
 
